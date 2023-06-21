d7677a1f082db343ed537653f0e8541e263845df
5ag5 <agonzalez555.gg@gmail.com>
2023-06-20 20:26:38 -0500

    version1

---
+++ src/main/java/inventarios/com/Sistema/Inventarios/SistemaInventariosApplication.java
@@ -0,0 +1,13 @@
+package inventarios.com.Sistema.Inventarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaInventariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaInventariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inventarios/com/Sistema/Inventarios/SistemaInventariosApplicationTests.java
@@ -0,0 +1,13 @@
+package inventarios.com.Sistema.Inventarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaInventariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
